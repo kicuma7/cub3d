a534e149cfab94961b930c4dc9260b6f954b0e07
kicuma7 <josephkicuma7@gmail.com>
2025-05-01 11:15:09 +0100

    feat: Compiling with correct Makefile

--- Makefile
+++ Makefile
@@ -1,1 +1,32 @@
+NAME = cub3d
+CC = cc
+CFLAGS = -Wall -Wextra -Werror -g3
+LIBS_FLAGS = -lft -L./libs/libft -lmlx -L./libs/mlx -Imlx -lXext -lX11 -lm -lz
 
+SRC_F = ./src/
+
+OBJ_F = ./.obj/
+
+FILES = main
+
+SRCS = $(addprefix $(SRC_F), $(addsuffix .c, $(FILES)))
+OBJS = $(addprefix $(OBJ_F), $(addsuffix .o, $(FILES)))
+
+$(NAME): $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) $(LIBS_FLAGS) -o $(NAME)
+
+$(OBJ_F)%.o: $(SRC_F)%.c
+	mkdir -p $(OBJ_F)
+	$(CC) $(CFLAGS) $(LIBS_FLAGS) -c $< -o $@
+
+all: $(NAME)
+
+clean:
+	$(RM) $(OBJS)
+
+fclean: clean
+	$(RM) $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ inc/cub.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jquicuma <jquicuma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 10:32:03 by jquicuma          #+#    #+#             */
+/*   Updated: 2025/05/01 10:58:04 by jquicuma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB_H
+# define CUB_H
+
+# include "../libs/libft/inc/libft.h"
+# include "../libs/mlx/mlx.h"
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+#endif
